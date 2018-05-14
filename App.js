e0032214d6a8ab6b9b3da108b86dc2cb857e096d
jhyuk <akajhyuk@gmail.com>
2018-05-14 17:19:09 +0900

    Input & Todo Components

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import ToDo from "./ToDo";
+
 
 const { height, width } = Dimensions.get("window")
 
 export default class App extends React.Component {
+
+  state = {
+    newToDo: ""
+  };
+
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>To Do List</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"}/>
+          <TextInput 
+          style={styles.input} 
+          placeholder={"New To Do"} 
+          value={newToDo} 
+          onChangeText={this._createNewToDo} 
+          placeholderTextColor={"#999"} 
+          returnKeyType={"done"} 
+          autoCorrect={false} />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
+  }
+  _createNewToDo = text => {
+    this.setState({
+      newToDo: text
+    })
   }
 }
 
@@ -50,5 +73,11 @@ const styles = StyleSheet.create({
         elevation: 3
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 20
   }
 });
