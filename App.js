3601afcba23c3b5201c9feeb99581a7025c558d6
jhyuk <akajhyuk@gmail.com>
2018-05-15 12:45:57 +0900

    coding the todo list item style

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default class App extends React.Component {
           placeholderTextColor={"#999"} 
           returnKeyType={"done"} 
           autoCorrect={false} />
-          <ScrollView>
+          <ScrollView contentContainerStyle={styles.todostyle}>
             <ToDo />
           </ScrollView>
         </View>
@@ -79,5 +79,8 @@ const styles = StyleSheet.create({
     borderBottomColor: "#bbb",
     borderBottomWidth: 1,
     fontSize: 20
+  },
+  todostyle: {
+    alignItems: "center"
   }
 });
