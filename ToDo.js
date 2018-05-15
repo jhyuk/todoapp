a04aa8eea33b390bf50fda84bd2eef9c8cb2b5c8
jhyuk <akajhyuk@gmail.com>
2018-05-15 16:10:54 +0900

    styling the Todo Component

--- ToDo.js
+++ ToDo.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native"
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 
 
 const { width, height } = Dimensions.get("window");
@@ -10,13 +11,38 @@ export default class ToDo extends Component {
         isCompleted: false
     };
     render(){
-        const { isCompleted } = this.state;
+        const { isCompleted, isEditing } = this.state;
         return(
             <View style={styles.container}>
-                <TouchableOpacity onPress={this._toggleComplete}>
-                    <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
-                </TouchableOpacity>
-                <Text style={styles.text}>Hello I'm a To Do</Text>
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
+                    </TouchableOpacity>
+                    <Text style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}>
+                        Hello I'm a To Do</Text>
+                </View>
+                {isEditing ? (
+                    <View style={styles.actions}>
+                        <TouchableOpacity onPressOut={this._finishEditing}>
+                            <View style={styles.actionContainer}>
+                                <FontAwesome name="check" size={30} />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._startEditing}>
+                                <View style={styles.actionContainer}>
+                                    <MaterialIcons name="mode-edit" size={30} />
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <FontAwesome name="close" size={30} color={"red"}/>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
             </View>
         )
     }
@@ -27,6 +53,16 @@ export default class ToDo extends Component {
             }
         })
     }
+    _startEditing = () => {
+        this.setState({
+            isEditing: true
+        })
+    }
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false
+        })
+    }
 }
 
 const styles = StyleSheet.create({
@@ -35,7 +71,8 @@ const styles = StyleSheet.create({
         borderBottomColor: "#bbb",
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: "row",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent: "space-between"
     },
     circle: {
         width: 30,
@@ -45,14 +82,34 @@ const styles = StyleSheet.create({
         marginRight: 20
     },
     completedCircle: {
-        borderColor: "red"
+        borderColor: "#bbb"
     },
     uncompletedCircle: {
-        borderColor: "#bbb"
+        borderColor: "red"
     },
     text: {
         fontWeight: "600",
         fontSize: 20,
         marginVertical: 20
+    },
+    completedText: {
+        color: "#bbb",
+        textDecorationLine: "line-through"
+    },
+    uncompletedText: {
+        color: "#353535"
+    },
+    column:{
+        flexDirection: "row",
+        alignItems: "center",
+        width: width / 2,
+        justifyContent: "space-between"
+    },
+    actions: {
+        flexDirection: "row",
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10
     }
 })
